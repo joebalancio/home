5c110fe143bbc3935fdeb9321c79bd77587c07aa
Joseph Balancio <jlbalancio@gmail.com>
2015-10-26 20:56:03 -0700

    Update git bash prompt

--- resources/git.bash
+++ resources/git.bash
@@ -2,10 +2,11 @@
 [[ -s $HOME/projects/git/contrib/completion/git-prompt.sh ]] && source $HOME/projects/git/contrib/completion/git-prompt.sh
 [[ -s $HOME/projects/git/contrib/completion/git-completion.bash && -n "$BASH_VERSION" ]] && source $HOME/projects/git/contrib/completion/git-completion.bash
 export GIT_PS1_SHOWCOLORHINTS=1
-export GIT_PS1_SHOWUPSTREAM="verbose name git" #auto|verbose|name|legact|git|svn
-export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUPSTREAM="auto" #auto|verbose|name|legact|git|svn
+#export GIT_PS1_SHOWDIRTYSTATE=1
+#export GIT_PS1_SHOWSTASHSTATE=1
 #export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_DESCRIBE_STYLE="branch" #contains|branch|describe|default
+#export GIT_PS1_DESCRIBE_STYLE="branch" #contains|branch|describe|default
 
 if hash hub
 then
