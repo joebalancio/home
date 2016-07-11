cfd3734ddb2bee24143a15fb0033edcb0f56bb30
Joseph Balancio <jlbalancio@gmail.com>
2016-07-11 09:39:22 -0700

    Update aliases

--- resources/aliases.bash
+++ resources/aliases.bash
@@ -1,10 +1,8 @@
-if [ -n "ZSH_VERSION" ]
-then
-  return
-fi
-
 # some more ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+# Neovim Takeover!
+alias vim="nvim"
+alias vi="nvim"
