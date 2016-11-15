52a3e8783147cf8bd34ba39abfa2b37a8b984373
Joseph Balancio <jlbalancio@gmail.com>
2016-11-14 20:40:31 -0800

    Add auto completion for tmuxinator

--- resources/tmux.bash
+++ resources/tmux.bash
@@ -1,3 +1,5 @@
 # tmux
 PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
 
+# tmuxinator
+[[ -s $HOME/projects/tmuxinator/completion/tmuxinator.bash ]] && source $HOME/projects/tmuxinator/completion/tmuxinator.bash
