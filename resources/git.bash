00421599ff025b987a646a7478df0cf9cc0b4454
Joe Balancio <jlbalancio@gmail.com>
2018-01-08 09:30:02 -0800

    git: add greport alias
    another way to view a git commits

--- resources/git.bash
+++ resources/git.bash
@@ -8,9 +8,11 @@ export GIT_PS1_SHOWUPSTREAM="auto" #auto|verbose|name|legact|git|svn
 #export GIT_PS1_SHOWUNTRACKEDFILES=1
 #export GIT_PS1_DESCRIBE_STYLE="branch" #contains|branch|describe|default
 
-if hash hub > /dev/null
+if hash hub 2> /dev/null
 then
   eval "$(hub alias -s)"
   alias prdev='git pull-request -a joebalancio -b dev'
   source $HOME/projects/hub/etc/hub.bash_completion.sh
 fi
+
+alias greport="git log --format='%cd %Cgreen %s %Cred %D %Creset' --date=short --all --since=2.days.ago --author=jlbalancio"
