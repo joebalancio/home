4e8bde96296b607531f78fa55ed8669979be04fe
Joe Balancio <jlbalancio@gmail.com>
2022-02-18 19:21:19 -0800

    Don't use neovim

--- resources/aliases.zsh
+++ resources/aliases.zsh
@@ -3,11 +3,6 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-# Neovim Takeover!
-alias vim="nvim"
-alias vi="nvim"
-alias v="nvim" # Super lazy
-
 # Emacs
 alias e="TERM=xterm-256color emacs -nw"
 alias ec="TERM=xterm-256color emacsclient -c -t"
