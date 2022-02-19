82d0c96416a1642672bcafe24c8467b26407770e
Joe Balancio <jlbalancio@gmail.com>
2022-02-18 19:06:10 -0800

    Clean up prompt

--- resources/prompt.zsh
+++ resources/prompt.zsh
@@ -2,10 +2,7 @@
 export CLICOLOR=1
 export LSCOLORS=ExxxxxDxBxegedabxxacad
 
-[ -n "$BASH_VERSION" ] && export PROMPT_COMMAND='__git_ps1 "\w" "\\\$ "'
-
 # History
-#export HISTCONTROL=ignoredups
 setopt HIST_IGNORE_DUPS
 
 ### PROMPT
