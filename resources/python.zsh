1ecefdefc34f774176a6473d1b15dd6a1193ecea
Joe Balancio <jlbalancio@gmail.com>
2022-03-11 11:58:26 -0800

    Run pyenv init

--- resources/python.zsh
+++ resources/python.zsh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
 
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="$PATH:$HOME/.poetry/bin"
 
